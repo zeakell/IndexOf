5a0c90d82a974bf22ca3f496d3abfa4e4865e90e
Kevin Harada <59675191+zeakell@users.noreply.github.com>
2025-01-01 16:13:26 +0700

    Add files via upload

---
+++ WebserverForIndexof.py
@@ -0,0 +1,17 @@
+
+import http.server
+import socketserver
+
+# Define the handler to use for incoming requests
+handler = http.server.SimpleHTTPRequestHandler
+
+# Set the port number for the server
+port = 9898
+
+# Create the server and bind it to the specified port
+httpd = socketserver.TCPServer(("", port), handler)
+
+print(f"Serving on port {port}")
+
+# Run the server indefinitely
+httpd.serve_forever()
